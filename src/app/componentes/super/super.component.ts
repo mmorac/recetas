eb4d72b0e44fabc37059da631d9a803ba3e027cb
Mario Mora <marioemorac@gmail.com>
2024-07-24 13:48:15 +0200

    Agregando cambio de supermercado
    Se pueden arrastrar ítemes hacia un supermercado u otro

--- src/app/componentes/super/super.component.ts
+++ src/app/componentes/super/super.component.ts
@@ -17,12 +17,38 @@ export class SuperComponent {
   getListas():void{
     this.superServicio.getListas().subscribe(data => {
       this.listas = data;
-      console.log(this.listas);
     })
   }
 
   ngOnInit(){
     this.getListas();
+  }
+
+  draggedItem:any;
+  draggedFromIndex:number = -1;
+
+  dragStart(event:DragEvent, item:any){
+    this.draggedItem = item;
+    const target = event.target as HTMLElement;
+    const indexStr = target.getAttribute('data-index');
+    this.draggedFromIndex = indexStr? +indexStr : -1;
+  }
+
+  allowDrop(event: DragEvent){
+    event.preventDefault();
+  }
+
+  drop(event:DragEvent, toIndex:number){
+    event.preventDefault();
+    if(this.draggedItem && this.draggedFromIndex !== undefined){
+      const fromList = this.listas[this.draggedFromIndex];
+      fromList.items = fromList.items.filter(item => item !== this.draggedItem);
+
+      const toList = this.listas[toIndex];
+      toList.items.push(this.draggedItem);
+      this.draggedItem = null;
+      this.draggedFromIndex = -1;
+    }
   }
 
 }
